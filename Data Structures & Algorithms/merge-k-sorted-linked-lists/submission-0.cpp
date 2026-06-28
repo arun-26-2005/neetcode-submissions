24e59049563f289bb098e843da44f69b73513bfd
Arunkumar D <197222499+arun-26-2005@users.noreply.github.com>
2026-06-28 16:53:43 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        ListNode* curr1=list1;
+        ListNode* curr2=list2;
+        ListNode* dummy=new ListNode();
+        ListNode* temp=dummy;
+        if(curr1==NULL)
+        {
+            return list2;
+        }
+        else if (curr2==NULL)
+        {
+            return list1;
+        }
+        else{
+            while(curr1!=NULL && curr2!=NULL)
+            {
+                if(curr1->val<=curr2->val)
+                {
+                    temp->next=curr1;
+                    temp=temp->next;
+                    curr1=curr1->next;
+                }
+                else{
+                     temp->next=curr2;
+                    temp=temp->next;
+                    curr2=curr2->next;
+
+                }
+                
+            }
+
+            while(curr1!=NULL)
+            {
+                temp->next=curr1;
+                temp=temp->next;
+                curr1=curr1->next;
+            }
+            while(curr2!=NULL)
+            {
+                temp->next=curr2;
+                temp=temp->next;
+                curr2=curr2->next;
+            }
+        }
+        return dummy->next;
+        
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()>=2)
+        {
+        ListNode* result=mergeTwoLists(lists[0],lists[1]);
+        for(int i=2;i<lists.size();i++)
+        {
+            result=mergeTwoLists(result,lists[i]);
+        }
+        return result;
+        }
+        else if(lists.size()==1)
+            return lists[0];
+        else
+            return NULL;
+        return NULL;
+        
+    }
+};
