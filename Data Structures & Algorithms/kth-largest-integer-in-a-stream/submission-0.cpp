8ffbf515336e8bf1b0e4946860341b4486bb0954
Arunkumar D <197222499+arun-26-2005@users.noreply.github.com>
2026-07-02 12:40:31 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,32 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int num;
+    KthLargest(int k, vector<int>& nums) {
+        num=k;
+        for(auto x: nums)
+        {
+            pq.push(x);
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }        
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>num)
+        {
+            pq.pop();
+        }
+        return pq.top();
+        
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
